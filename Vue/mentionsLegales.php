8d652c11bfef792f95a60602b7cb105c9faf785a
MathiasBenner <50447720+MathiasBenner@users.noreply.github.com>
2021-03-29 11:14:12 +0200

    Placeholders à changer

---
+++ Vue/mentionsLegales.php
@@ -0,0 +1,199 @@
+<?php
+$titre="Mokytis";
+
+ob_start();
+?>
+
+<div id=”conteneur”>
+<section>
+	<h2>Conditions d'utilisation de Mokytis (Mise à jour du 22 mars 2021)</h2>
+	<p>
+		Merci de prendre un instant pour passer en revue les règles détaillées ci-dessous. <br/><br/>
+		L'utilisation des forums Mokytis, en tant que membre ou simple utilisateur, implique l'adhésion entière et inconditionnelle aux conditions ci-dessous.
+	</p>
+	<hr/>
+	<p>
+		<b>ART1. Présentation.</b>
+	</p>
+	<p>
+		Mokytis est un site Internet français gratuit, ouvert à tous. Les frais relatifs à l'accès au site (connexion Internet et équipements informatiques) quels qu'ils soient, sont à la charge de l'utilisateur.
+		<br/><br/>
+
+		En pénétrant sur ce forum, vous certifiez être âgé de 18 ans ou plus.
+	</p>
+	<hr/>
+	<p>
+		<b>ART2. Données personnelles.</b>
+	</p>
+	<p>
+		Les informations à renseigner à l'inscription ne seront utilisées qu'à titre indicatif. Aucune information civile (nom, adresses, numéros de téléphone) n'est exigée.
+		<br/><br/>
+		Pseudo, mot de passe et adresse e-mail sont les seules informations à renseigner obligatoirement à l'inscription.
+		<br/><br/>
+		Chaque membre garantit sur l'honneur l'exactitude des informations fournies.
+		<br/><br/>
+		Chaque membre s'engage à ne créer qu'un seul compte.
+		<br/><br/>
+		Conformément à la loi « informatique et libertés » du 6 janvier 1978 modifiée en 2004, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent, que vous pouvez exercer en vous adressant à Mokytis / contact@mokytis.com
+	</p>
+	<hr/>
+	<p>
+		<b>ART3. Contenu de vos participations.</b>
+	</p>
+	<p>
+		Par participation, nous sous entendons l'ensemble des services proposés (Forum, quiz, chat, cours, … )
+		<br/><br/>
+		Vos participations ne devront:
+		<br/><br/>
+		contenir aucun caractère incitant à la haine,
+		contenir aucun caractère diffamant,
+		contenir aucun caractère menaçant,
+		contenir aucun caractère illégal ou allant à l'encontre de n'importe quelle loi d'une quelconque manière,
+		contenir aucun caractère publicitaire; Il est à noter que les liens de sites Internet rentrent dans le cadre de la publicité. Les liens vers d'autres sites seront tolérés selon l'appréciation des administrateurs et des modérateurs du forum. contenir aucune propagande (religieuse, politique, … ),
+		contenir aucun caractère sexuel ( Erotique, Pornographique, poses obscènes ou provocantes, …),
+		contenir aucune information personnelle (nom, adresse, e-mail, téléphone …) , mise à part dans le cadre d'une discussion portant sur des personnes publiques. L'utilisation d'abréviations est tolérée. pas être insultant ou manquer de respect à d'autres participants
+		Tout message de recrutement / prospection / partenariat sera considéré comme publicitaire. En cas de besoin ou de demande spécifique, vous pouvez nous contacter via notre formulaire de contact.
+		<br/><br/>
+		L'utilisation que vous ferez des services ne devra pas être :
+		<br/><br/>
+		abusive (multiplication de messages, sujets...),
+		malhonnête (destinée à ne pas respecter les règles propres à chaque service).
+		Vous vous engagez, par l'utilisation de ce service, à ne pas publier de contenu ne se conformant pas aux droits d'auteur ou aux licences d'exploitation.
+		<br/><br/>
+		Vous vous engagez à ne pas poster de contenu en rapport avec l'utilisation de contenus frauduleux, pirates, ou contrefaits.
+		<br/><br/>
+		Vous vous engagez à ne pas poster de contenu ne respectant pas la législation française en vigueur.
+		<br/><br/>
+		Vous vous engagez enfin, sur l'honneur, à ne pas contrevenir aux deux points suscités en abusant de l'utilisation de notre politique sur le contenu.
+		<br/><br/>
+		De plus, Mokytis n'étant pas la vitrine d'un parti politique ou d'une religion particulière, nous n'accepterons aucune propagande.
+	</p>
+	<hr/>
+	<p>
+		<b>ART5. Messagerie privée.</b>
+	</p>
+	<p>
+		Comme son nom l'indique la messagerie est privée, cela signifie que vous êtes les seuls à pouvoir consulter le contenu s'y trouvant.
+		<br/><br/>
+		En cas de problème  (Insultes, menaces, spams, ... ) il vous est possible d'ajouter un modérateur, ou un administrateur à la conversation. Cela nous permettra d'en prendre connaissance et de pouvoir prendre les mesures nécessaires
+		<br/><br/>
+		La messagerie étant un espace privé, vous ne devez en aucun cas révéler le contenu d'un message sur le forum sans avoir le consentement des différents participants à la conversation.
+	</p>
+	<hr/>
+	<p>
+		<b>ART6. Sanction en cas de non-respect des conditions d'utilisation.</b>
+	</p>
+	<p>
+		En cas de non-respect du règlement, voici les sanctions possibles :
+		<br/><br/>
+		mise en garde par un membre de l'Equipe,
+		avertissement,
+		avertissement avec désactivation temporaire du compte,
+		bannissement du forum.
+		En fonction de la faute commise et de l'ancienneté du membre, la sanction peut varier.
+		<br/><br/>
+		Un nouveau membre multipliant les messages publicitaires sera immédiatement banni.
+		<br/><br/>
+		Dans le cas où un membre est averti, son avertissement lui sera retiré au minimum 1 mois après les faits reprochés, si son attitude a changé.
+		<br/><br/>
+		Au 5e avertissement, un membre est banni.
+		<br/><br/>
+		Un membre banni est définitivement banni et ne sera plus accepté sur notre plateforme.
+	</p>
+	<hr/>
+	<p>
+		<b>ART7. Responsabilités.</b>
+	</p>
+	<p>
+		<br/><br/>
+		La modération a posteriori ne permet pas a ForumFr d'être tenu responsable des messages postés, nous ne garantissons ni l'exactitude, ni la véracité, ni la valeur officielle d'aucun message.
+		<br/><br/>
+		ForumFr s'engage à intervenir en cas d'alerte dans les plus brefs délais.
+		<br/><br/>
+		Les outils de mise en forme, de mise en valeur, ou les zones de visualisation optimisées, comme celles utilisées en haut de chaque index, ne sont là qu'à titre indicatif. Celles-ci sont destinées à mettre en valeur certains messages, mais ne constituent en aucun cas une adhésion à leur contenu, une approbation ou un report des responsabilités.
+		<br/><br/>
+		Tout message contrevenant aux règles ci-présentes, à la loi ou à tout autre code de conduite, n'engage que la responsabilité de leur auteur.
+		<br/><br/>
+		Bien que contrôlé au mieux, avec une volonté d'optimiser les délais et les conditions de contrôle sur le contenu, par des ayants droit du forum, le terme "modération" ne signifie pas que ForumFr ou les ayants droit du forum disposent d'un contrôle total sur les messages du forum.
+		<br/><br/>
+		N'importe quel utilisateur qui estime qu'un message envoyé est répréhensible, est encouragé à nous contacter immédiatement par e-mail ou à l'aide de l'outil d'alerte mis à disposition.
+		<br/><br/>
+		Dans ce souci d'optimisation du contrôle sur le contenu, ForumFr s'autorise à supprimer, modifier, déplacer, tout message du forum qui serait jugé contrevenant au bon fonctionnement du forum, sans préavis, à supprimer, modifier ou suspendre tout compte de membre ou à déléguer ces droits à certains membres, groupes, définissant le terme "ayants droit" suscité.
+		<br/><br/>
+		Afin de respecter les présentes conditions d'utilisation, les adresses IPs sont enregistrées.
+		<br/><br/>
+		Dans l'optique du rôle qui leur est assigné, les ayants droit de ForumFr ne sont pas tenus de justifier leurs actions. Si une contestation semble nécessaire, celle-ci devra se faire obligatoirement par Message Privé. Le ou les membres de l'équipe contactés se réservent le droit de répondre au message.
+	</p>
+	<hr/>
+	<p>
+		<b>ART8. Droit Applicable.</b>
+	</p>
+	<p>
+		Toute contribution sur le site ForumFr.com est régie par le Code de la propriété Intellectuelle. Chaque participant restant le propriétaire de ses messages.
+		<br/><br/>
+		En publiant des contributions (Messages, Sujets, Articles, ...), vous autorisez, à titre gracieux, leur reproduction et leur diffusion sur les sites internet de la société ForumFr pour le monde entier et pour la durée la plus longue prévue par la loi française en vigueur et vous autorisez la société ForumFr à référencer le contenu de vos contributions sur les différents moteurs de recherche.
+		<br/><br/>
+		Vous autorisez les différents membres à citer l'intégralité de vos messages en utilisant le système de citation mis à disposition.
+		<br/><br/>
+		Vous renoncez à poursuivre la société ForumFr suite à un problème ou dommage causé par un autre utilisateur en violation des conditions générales d'utilisation.
+		<br/><br/>
+		Conformément à l'article 6-IV de la loi n°2004-575 et au Décret n°2007-1527 toute personne physique ou morale pourra bénéficier d'un droit de réponse.
+	</p>
+	<hr/>
+	<p>
+		<b>ART9. Cookies.</b>
+	</p>
+	<p>
+		Le site ForumFr.com est amené à enregistrer des cookies sur votre navigateur afin de conserver vos préférences de navigation et pour réaliser des statistiques ( Google Analytics).
+		<br/><br/>
+		Vous pouvez vous opposer à cet enregistrement via les préférences de votre navigateur.
+	</p>
+	<hr/>
+	<p>
+		<b>ART10. Modification des Conditions Générales d’Utilisation</b>
+	</p>
+	<p>
+		Les CGU de ForumFr sont amenées à être modifiées régulièrement.
+		<br/><br/>
+		Un message posté dans le forum Annonces vous préviendra de toute modification
+		<br/><br/>
+		Leur acceptation est obligatoire pour accéder aux différents services.
+	</p>
+	<hr/>
+	<p>
+		<b>ART11. Politique sur le contenu.</b>
+	</p>
+	<p>
+		Les us et coutumes des forums francophones, de manière générale, autorisent (tacitement et) majoritairement le colportage de contenu à caractère informatif.
+		<br/><br/>
+		ForumFr bénéficie des mêmes dispositions et le contenu à caractère informatif publié par les utilisateurs est donc généralement toléré.
+		<br/><br/>
+		ForumFr s'engage moralement à ne pas autoriser la publication de travaux rédactionnels ou tendant à un effort de rédaction, protégés par le droit d'auteur, sans accord des dits auteurs.
+		<br/><br/>
+		Si néanmoins un éditeur informatif, site internet, ou toute autre source journalistique souhaitait attirer notre attention sur certains contenus publiés par les utilisateurs, nous l'encourageons à nous contacter.
+		<br/><br/>
+	</p>
+	<hr/>
+	<p>
+		<b>Mentions Légales</b>
+	</p>
+	<p>
+		Conformément à la loi « informatique et libertés » du 6 janvier 1978 modifiée en 2004, vous bénéficiez d’un droit d’accès et de rectification aux informations qui vous concernent, que vous pouvez exercer en vous adressant à Mokytis dont les coordonnées sont ci-dessous.
+		<br/><br/>
+		Vous pouvez également, pour des motifs légitimes, vous opposer au traitement des données vous concernant.
+		<br/><br/>
+		Mokytis est la propriété de la société Mokytis :
+		RCS : 
+		Gérant : Ilya Meignan-Masson 
+		Tel/Fax : 
+		Mèl : contact@mokytis.com
+		<br/><br/>
+		Mokytis est hébergé par ...:
+	</p>
+</section>
+</div>
+
+<?php
+$contenu=ob_get_clean();
+require 'template.php';
+?>
