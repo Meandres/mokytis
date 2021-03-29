4ab6d1837a981e74815a15d9ff5df9fd12b79a7f
MathiasBenner <50447720+MathiasBenner@users.noreply.github.com>
2021-03-29 12:33:28 +0200

    Add files via upload

--- Vue/template.php
+++ Vue/template.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8"/>
-	<link rel="stylesheet" href="Style/style.css"/>
+	<link rel="stylesheet" href="../Style/orangeIsTheNewBlack.css" media="screen">
 	<script src="//code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<title><?php $titre ?></title>
@@ -13,7 +13,7 @@
 		<h1> LOGO </h1>
 		<div id="header">
 		  <nav>
-		    <a href="#">Accueil</a>
+		    <a href="vueAccueil.php">Accueil</a>
 		    <a href="#">Forum</a>
 		    <a href="#">Cours</a>
 				<button type="button" name="button" id="profil"> profil</button>
@@ -30,6 +30,21 @@
 	<div id="contenu">
 		<?php echo $contenu ?>
 	</div>
+	<footer>
+		<div id="footer">
+		<p style="padding-top: 5px;">@ 2021 Mokytis. Tous droits réservés. 
+		<select class="themeButton" style="background-color: lightgrey; padding: 2px;">  
+			<option value="ClaBlack">Classic Black</option> 
+			<option value="NeoBlue">Neo Blue</option>
+		</select><br/><br/><br/>
+		<span class="footer_links" style="padding-bottom: 20px;">
+			<a href="nousContacter.php">Nous contacter</a> 
+		  	<a href="politiqueViePrivee.php">Politique de protection de la vie privée</a>
+		  	<a href="mentionsLegales.php">Conditions et mentions Légales</a> 
+		  </span>
+		</p>
+		</div>
+	</footer>
 	</div>
 </body>
 <script src="Javascript/javascript.js"></script>
