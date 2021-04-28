2f839923ec1444aeb34d1a191cca898e5607fae7
MathiasBenner <50447720+MathiasBenner@users.noreply.github.com>
2021-04-28 12:04:19 +0200

    Ajout pour affichage

--- Modele/ReponseForum.php
+++ Modele/ReponseForum.php
@@ -12,14 +12,14 @@ class ReponseForum{
 	public function getSujet(){ return $this->sujet; }
 
 	public function getAuteur(){ return $this->auteur; }
-	
+
 	public function getDatePubli(){ return $this->datePubli; }
-	
+
 	//set
 	public function setIdRepForum($ID){ $this->idRepForum=$ID; }
-	
+
 	public function setContenu($Contenu){ $this->contenu=$Contenu; }
-	
+
 	public function setCours($Sujet){ $this->sujet=$Sujet; }
 
 	public function setAuteur($Auteur){ $this->auteur=$Auteur; }
@@ -28,7 +28,7 @@ class ReponseForum{
 
 	//constructeur par défault (peut-être à enlever ?)
 	public function __construct(){}
-	
+
 	//constructeur pour une requete dans la base
 	protected function remplirBD(array $row){
 		$this->idRepForum=$row['idRepForum'];
@@ -45,6 +45,18 @@ class ReponseForum{
 		$this->sujet=$Sujet;
 		$this->auteur=$Auteur;
 		$this->datePubli=$DateP;
+	}
+
+	public function afficherApperçu(){
+		$affichage = "<a class='reponse'><div class='appercuSujet'><h3>".$this->contenu."</h3>".$this->datePubli."<br> ".$this->auteur."</div></a>";
+		return $affichage;
+	}
+
+	//fabrique de classe (à partir d'une requete à la base)
+	public static function avecBD(array $row){
+		$instance=new self();
+		$instance->remplirBD($row);
+		return $instance;
 	}
 
 	//fabrique de classe (pour la creation d'une reponse a un sujet du forum)
